52328f92902f4baf29563eee57c08a53b0e69577
lukas-santiago <lukas.asant@gmail.com>
2022-05-16 20:50:41 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fesa-ec7-censos-web';
+}
